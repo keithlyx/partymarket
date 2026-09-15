f12cb19b31f3eef84a71248f583177bff4564abd
Keith <keith.lee.2021@scis.smu.edu.sg>
2026-09-15 21:13:01 +0800

    chore(database): align initialization scripts with models

--- databases/orders.sql
+++ databases/orders.sql
@@ -6,8 +6,8 @@ DROP TABLE IF EXISTS order_venue;
 DROP TABLE IF EXISTS order_items;
 DROP TABLE IF EXISTS orders;
 CREATE TABLE IF NOT EXISTS orders (
-  order_id VARCHAR(100) NOT NULL PRIMARY KEY,
-  user_id VARCHAR(100) NOT NULL,
+  order_id VARCHAR(255) NOT NULL PRIMARY KEY,
+  user_id VARCHAR(255) NOT NULL,
   total_amount DECIMAL(10, 2) NOT NULL,
   order_datetime VARCHAR(255) NOT NULL,
   delivery_address VARCHAR(255) NOT NULL,
@@ -15,9 +15,8 @@ CREATE TABLE IF NOT EXISTS orders (
   order_status VARCHAR(100) NOT NULL
 );
 
-DROP TABLE IF EXISTS order_items;
 CREATE TABLE IF NOT EXISTS order_items (
-  order_id VARCHAR(100) NOT NULL ,
+  order_id VARCHAR(255) NOT NULL ,
   item_id VARCHAR(255) NOT NULL,
   item_quantity INT NOT NULL,
   item_price DECIMAL(10, 2) NOT NULL,
@@ -25,9 +24,8 @@ CREATE TABLE IF NOT EXISTS order_items (
   FOREIGN KEY (order_id) REFERENCES orders (order_id)
 );
 
-DROP TABLE IF EXISTS order_venue;
 CREATE TABLE IF NOT EXISTS order_venue (
-  order_id VARCHAR(100) NOT NULL,
+  order_id VARCHAR(255) NOT NULL,
   venue_id VARCHAR(255) NOT NULL,
   venue_price DECIMAL(10, 2) NOT NULL,
   venue_datetime VARCHAR(255) NOT NULL,
